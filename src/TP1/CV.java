f178e8417c828a7dfc8524f599bf1ab06cb077ce
Antoine La Boissiere <antoine.laboissiere@gmail.com>
2020-02-03 16:16:02 -0500

    v0.0.3

--- src/TP1/CV.java
+++ src/TP1/CV.java
@@ -6,14 +6,32 @@ public class CV {
 	String prenom;
 	String formation;
 	int experiencesTravail;
-	String[] competences;
+	String competences;
 	String attentes;
 	
 	public CV() {
 		
 	}
 	
-	public CV(String nom, String prenom, String formation, int experiancesTravail, String[] competences, String attentes) {
+	public CV(String nom, String prenom, String formation, int experiancesTravail, String competences, String attentes) {
+		
+		this.nom = nom;
+		this.prenom = prenom;
+		this.formation = formation;
+		this.experiencesTravail = experiancesTravail;
+		this.competences = competences;
+		this.attentes = attentes;
+		
+	}
+	
+	public void affiche(CV cv) {
+		
+		System.out.println("Nom : " + cv.nom + "\n" +
+						   "Prenom : " + cv.prenom + "\n" +
+						   "Formation : " + cv.formation + "\n" +
+						   "Expériances de travail : " + cv.experiencesTravail + "\n" +
+						   "Compétences : " + cv.competences + "\n" +
+						   "Attentes : " + cv.attentes);
 		
 	}
 	
--- src/TP1/Principal.java
+++ src/TP1/Principal.java
@@ -4,9 +4,18 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
-
+		CV cv = new CV("La Boissière", "Antoine", "Étudiant", 1, "Sup", "Rien");
+		
+		cv.affiche(cv);
+		
+		cv = new CV("La Boissière", "Antoine", "Étudiant", 1, "Sup", "Rien");
+		
+		cv.affiche(cv);
+		
+		cv = new CV("La Boissière", "Antoine", "Étudiant", 1, "Sup", "Rien");
+		
+		cv.affiche(cv);
+		
 	}
-	
-	
 
 }
